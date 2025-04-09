69ef5396b98e4471d4f6a2f8c4ed5a67f640bc23
h31w1tty <heiwitty.16@gmail.com>
2025-04-09 11:49:59 -0300

    estilização dos botoes e background

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const listaVersoes = [
-        { nome: "Portfólio V1", link: "https://h31w1tty.github.io/portifoliov1" },
-        { nome: "Portfólio V2", link: "https://h31w1tty.github.io" },
+        { nome: "Primeira versão", link: "https://h31w1tty.github.io/portifoliov1" },
+        { nome: "Segunda versão", link: "https://h31w1tty.github.io" },
         { nome: "Em Breve", link: "#" }
     ];
 
@@ -10,27 +10,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
     listaVersoes.forEach(versao => {
 
-        const divItem = document.createElement("div");
-        divItem.classList.add("divItem");
+        const divItem = document.createElement("div");//atribui a div dos itens a variável
+        divItem.classList.add("divItem");//coloca classe para estilização a div dos itens usando a variavel para fazer isso
+
+        divItem.onclick = function(){
+            window.location.href = versao.link;
+        }
         const p = document.createElement("p");
         p.textContent = versao.nome;
 
-        const linkPC = document.createElement("a");
-        linkPC.classList.add("pc");
-        linkPC.href = versao.link;
-        linkPC.textContent = versao.link;
-
-        const linkCel = document.createElement("a");
-        linkCel.classList.add("celular");
-        linkCel.href = versao.link;
-        linkCel.textContent = "visitar";
-
         divItem.appendChild(p);
-        divItem.appendChild(linkPC);
-        divItem.appendChild(linkCel);
         divItem.appendChild(document.createElement("br"));
         divItem.appendChild(document.createElement("br"));
         divItem.appendChild(document.createElement("br"));
         divPrincipal.appendChild(divItem);
+
+        function abrirLink(onclick){
+
+        }
     });
 });
